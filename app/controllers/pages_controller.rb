fa18f2550d36372e1c2fd58b4f65d412a5e25164
maximilian Woltersdorf <m.woltersdorf@hotmail.de>
2011-10-25 16:46:27 +0200

    fb start2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
+  require 'fb_graph'
   
   def index
  	user = FbGraph::User.fetch('matake')
+ 	@name = user.name
+ 	@picture = user.picture
   end
   
 end
