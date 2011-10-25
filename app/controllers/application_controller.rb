4f3f4e8286c41d69293ae91757b641f396a19baa
maximilian Woltersdorf <m.woltersdorf@hotmail.de>
2011-10-25 17:30:43 +0200

    new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+  include Facebooker2::Rails::Controller
 
 private
+
+def fb_permission_check
+	
+	render 'pages/fb_permission'
+	
+end
 
 	
 
