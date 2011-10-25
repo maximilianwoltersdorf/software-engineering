338e50b56498bc27f2bf9ee757c07d51a322a689
maximilian Woltersdorf <m.woltersdorf@hotmail.de>
2011-10-25 17:04:29 +0200

    fb graph

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,15 @@ class PagesController < ApplicationController
 
   
   def index
- 	
-  end
+ 	auth = FbGraph::Auth.new(280822525282026, ec11c6fcb8459f52b9e4c2640116443d)
+	auth.from_signed_request(params[:signed_request])
+		if auth.authorized?
+  			# If authorized, the auth has user and access_token.
+  		auth.user.fetch
+		else
+  			# First time user, show "Connect" button here.
+  		render 'pages/fb_permission'
+		end
+ 	end
   
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,6 @@
 class UserController < ApplicationController
+	
+	def new
+		
+	end
 end
