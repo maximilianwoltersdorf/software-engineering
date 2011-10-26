6e48bdd555a39ea9ad05fc9037900414389c116a
maximilian Woltersdorf <m.woltersdorf@hotmail.de>
2011-10-26 19:18:11 +0200

    db new

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,7 +6,10 @@ class UserController < ApplicationController
 			session[:user_name] = User.find_by_name(current_facebook_user.name).name
 			redirect_to root_path
 		else
-			@user = User.create(:name => current_facebook_user.name)
+			@user = User.create(:name => current_facebook_user.name,
+								:gender => current_facebook_user.gender,
+								:birthday => current_facebook_user.birthday,
+								:email => current_facebook_user.email)
 			cookies.permanent[:user_name] = @user.name
 			session[:user_name] = @user.name
 			redirect_to root_path
