38760621143e0fd00d0db3955a4be84954217207
maximilian Woltersdorf <m.woltersdorf@hotmail.de>
2011-10-26 16:10:00 +0200

    22

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
 	before_filter 'fb_permission_check'
   
   def index
-		
+		current_facebook_user.fetch
+		@interest = current_facebook_user
   end
   
 end
