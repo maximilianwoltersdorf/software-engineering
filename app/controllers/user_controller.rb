fbcd5c5f6dddff2c5bc25b4573ebab5feaf198cf
maximilian Woltersdorf <m.woltersdorf@hotmail.de>
2011-10-26 12:18:57 +0200

    fb login 12

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,7 +3,7 @@ class UserController < ApplicationController
 	def new
 		current_facebook_user.fetch
 		if User.find_by_name(current_facebook_user.name) != nil
-			session[:user_name] = User.where(:name => current_facebook_user.name)
+			session[:user_name] = User.where(:name => current_facebook_user.name).name
 			redirect_to root_path
 		else
 			@user = User.create(:name => current_facebook_user.name)
