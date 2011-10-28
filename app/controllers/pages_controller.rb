5fde063428790eb57ec7cecb1b88bd22a1986209
maximilian Woltersdorf <m.woltersdorf@hotmail.de>
2011-10-28 16:38:42 +0200

    ghg

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
 	before_filter 'fb_permission_check'
+
   
   def index
-	@me = current_facebook_user.fetch
+  	client = Mogli::Client.new("280822525282026|--TcorKnfsLncITMeKZF24crcu0")
+	@me = Mogli::User.find(100000405507618,client)
   end
   
 end
