3826ac2af9e834f0b9d3ffce7ae023120feaa21e
maximilian Woltersdorf <m.woltersdorf@hotmail.de>
2011-10-31 12:56:08 +0100

    random

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,8 @@
 class Like < ActiveRecord::Base
 	def self.random
 		size = Like.count
-		@like1 = Like.find(rand(size))
-		@like2 = Like.find(rand(size))
+		@like1 = Like.find(1+rand(size))
+		@like2 = Like.find(1+rand(size))
 		if @like1 == @like2
 			@like2 = Like.find(rand(size))
 		end
