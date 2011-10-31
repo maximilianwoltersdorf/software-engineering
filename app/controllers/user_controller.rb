dd9d7c7deb33b1c47601a2d478178167011428af
maximilian Woltersdorf <m.woltersdorf@hotmail.de>
2011-10-31 16:17:05 +0100

    gj

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -21,10 +21,11 @@ class UserController < ApplicationController
 
 	def create
 		current_facebook_user.likes.each do |item|
-		
+			if Like.find_by_group(item.name)
+			else
 			Like.create(:group => item.name,
-						:count => "0")
-			
+						:count => "0")	
+			end
 		end
 		redirect_to root_path
 
