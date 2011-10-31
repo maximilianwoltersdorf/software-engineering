22e2fdc7648adadf08eaa236857bbd3f581105dd
maximilian Woltersdorf <m.woltersdorf@hotmail.de>
2011-10-31 11:26:49 +0100

    22

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,9 +1,7 @@
 class GameController < ApplicationController
 	require 'mogli'
   def index
-  	client = Mogli::Client.new("280822525282026|--TcorKnfsLncITMeKZF24crcu0")
-  	current_facebook_user.fetch
-  	@myself_likes = current_facebook_user.likes
+  
 
   end
 
