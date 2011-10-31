0336c4edd47bdd26e67bffadc98c27cfc600b98a
maximilian Woltersdorf <m.woltersdorf@hotmail.de>
2011-10-31 13:16:54 +0100

    random

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,9 +1,10 @@
 class GameController < ApplicationController
 	require 'mogli'
   def index
-  size = Like.all.count
-  @like1 = Like.find(1+rand(size))
-  @like2 = Like.find(1+rand(size))
+  	Like.random
+  #size = Like.all.count
+  #@like1 = Like.find(1+rand(size))
+  #@like2 = Like.find(1+rand(size))
 
   end
 
