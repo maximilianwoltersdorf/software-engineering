74e8755f33d14606f6b92d2cf2cd27d3e998fe0c
maximilian Woltersdorf <m.woltersdorf@hotmail.de>
2011-10-31 10:23:34 +0100

    22

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,9 +1,9 @@
 class GameController < ApplicationController
 	require 'mogli'
   def index
-  	client = Mogli::Client.new("238845252805361|6a7pahTs-H4yeHgNq5PFIN5a5cQ")
-  	album = Mogli::Album.find(7807422276, client)
-	album.photos
+  	client = Mogli::Client.new("280822525282026|--TcorKnfsLncITMeKZF24crcu0")
+  	current_facebook_user.fetch
+
   end
 
   def new
