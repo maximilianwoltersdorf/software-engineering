ad161192e47fd18c9fc1ee2221b9830028feae2d
maximilian Woltersdorf <m.woltersdorf@hotmail.de>
2011-11-01 15:02:31 +0100

    22

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -16,10 +16,11 @@ class GameController < ApplicationController
 
   def show
     current_facebook_user.fetch
-
     current_facebook_user.likes.each do |item|
-    @user_like = Like.find_by_group(item.name)
-    end
+    @user_likes = item
+  end
+
+    
 
   end 
 
