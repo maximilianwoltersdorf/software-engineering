1cd0ddd8d41f70607ffe7d38164076dacfd79df0
maximilian Woltersdorf <m.woltersdorf@hotmail.de>
2011-11-03 09:47:32 +0100

    22

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -19,10 +19,9 @@ class GameController < ApplicationController
    alllikes     = Hash.new
       
     fbuser.likes.each do |like|
-      alllikes[like.name] = Like.find_by_group(like.name).count
+      @user_likes[like.name] = Like.find_by_group(like.name).count
     end
 
-    @user_likes = alllikes.sort.take(10)
       
     
     #@user_like
